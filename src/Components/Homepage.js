338fd8032ac4cf4111a5c644521668e860363bc1
Sushant <sushant.adhikari.619@gmail.com>
2021-04-09 19:31:59 +0545

    Homepage and Navbar

---
+++ src/Components/Homepage.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+import { useDispatch, useSelector } from 'react-redux';
+import { selectSignedIn, setSignedIn, setUserData } from '../features/userSlice';
+
+import "../styling/home.css";
+
+const Homepage = () => {
+    const dispatch = useDispatch();
+    const login = (response) => {
+        console.log(response);
+        dispatch(setSignedIn(true));
+        dispatch(setUserData(response.profileObj));
+    };
+
+    const isSignedIn = useSelector(selectSignedIn)
+
+    return (
+        <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
+            {!isSignedIn &&             
+            (<div className="login__message">
+                <h2>📖</h2>
+                <h1>A Learner's favourite place!</h1>
+                <p>
+                    We provide high quality online resource for reading blogs. Just sign up and start reading some quality blogs.
+                </p>
+                <GoogleLogin 
+                    clientId="830164986628-0hi0hqa3cc69o2k4vft2p4ocdjot22c0.apps.googleusercontent.com"
+                    render={(renderProps) =>(
+                          <button
+                          onClick={renderProps.onClick}
+                          disabled={renderProps.disabled}
+                          className="login__button"
+                          >
+                              Login with Google
+                          </button>
+                    )}
+                    onSuccess={login}
+                    onFailure={login}
+                    isSignedIn={true}
+                    cookiePolicy={"single_host_origin"}
+                />
+            </div>)}
+        </div>
+    )
+}
+
+export default Homepage
